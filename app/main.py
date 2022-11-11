e3154cf5955a6bb1cba024927421cd3db8fcf251
Jason Lunder <jlunder@zagmail.gonzaga.edu>
2022-11-11 11:02:50 -0800

    start deployment stuff. do env vars for keys

--- app/main.py
+++ app/main.py
@@ -5,21 +5,24 @@ import subprocess
 import json
 import datetime, time
 import redis
-
-
-# Define a list of valid API keys
-API_KEYS = [
-    "9d207bf0-10f5-4d8f-a479-22ff5aeff8d1",
-    "f47d4a2c-24cf-4745-937e-620a5963c0b8",
-    "b7061546-75e8-444b-a2c4-f19655d07eb8",
-]
+from pathlib import Path
+import os
 
 r = redis.Redis(host='localhost', port=8888, db=0)
+
+API_KEYS = os.environ['EVERYPROMPT_API_KEYS'].split(':')
 
 # Define the name of query param to retrieve an API key from
 api_key_query = APIKeyQuery(name="api-key", auto_error=False)
 # Define the name of HTTP header to retrieve an API key from
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+
+infolder = Path('./tmp/in')
+outfolder = Path('./tmp/out')
+if not infolder.exists():
+    infolder.mkdir(parents=True)
+if not outfolder.exists():
+    outfolder.mkdir(parents=True)
 
 
 def get_api_key(
@@ -53,22 +56,20 @@ app = FastAPI(title="JSON To TypeScript Converter")
 
 @app.post("/generate_typescript")
 async def generate_typescript(json_input: JSONInput):
-    #TODO: run jtd-codegen on inputted data, get inputted data
     json_string = json.dumps(json_input.json_schema)
     title = json_input.schema_title
     cached_result = r.get(json_string)
     if cached_result is not None:
-        print('USED CACHE')
         return {'typescript':cached_result} 
-    print('DID NOT USE CACHE')
-
-    with open ('./tmp/in/'+'.json', 'w') as fin:
+    t = str(time.time())
+    infile = Path('./tmp/in/'+title+'-'+t+'.json')
+    with open(str(infile), 'w') as fin:
         fin.write(json_string)
-    # folder = str(time.time()) #TODO: make better tmp folder creation/deletion
-    subprocess.Popen(["./lib/bin/jtd-codegen", "./tmp/in/in.json", "--root-name", str(json_input.schema_title), "--typescript-out", "./tmp/"])
-        # print(proc.stdout.read())
+    outfile = Path('./tmp/out/'+title+'-'+t+'.ts')
+    subprocess.Popen(["./lib/bin/jtd-codegen", str(infile), "--root-name", str(json_input.schema_title), "--typescript-out", str(outfile)])
+    outfile.unlink()
     generated_typescript = ''
-    with open("./tmp/index.ts") as fin:
+    with open(str(outfile), 'r') as fin:
         generated_typescript = '\n'.join(fin.readlines()[2:])
         r.set(json_string, generated_typescript) 
     return {'typescript':generated_typescript} 
